d7e6a741a146a1a3b37924a2fa714b09fe4ecb43
Angel Pincay <angel.pincay@intrado.com>
2022-08-23 19:40:42 -0500

    Initial commit

---
+++ infrastructure/Program.cs
@@ -0,0 +1,26 @@
+﻿using Pulumi;
+using Pulumi.Aws.Ec2;
+using Pulumi.Aws.Ec2.Inputs;
+using System.Collections.Generic;
+
+return await Deployment.RunAsync(() =>
+{
+   var ami = GetAmi.Invoke(new GetAmiInvokeArgs 
+   {
+       Owners = { },
+       MostRecent = true,
+       Filters = {
+           new GetAmiFilterArgs 
+           {
+               Name = "name",
+               Values = { "amzn-ami-hvm-*"}
+           }
+       }
+   });
+
+   // Export the name of the bucket
+   return new Dictionary<string, object?>
+   {
+      //["bucketName"] = bucket.Id
+   };
+});
