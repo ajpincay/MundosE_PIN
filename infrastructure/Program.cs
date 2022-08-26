67b91b7aea4899fcd9b7721b368dc7520c27ff0f
Angel Pincay <angel.pincay@intrado.com>
2022-08-25 19:10:19 -0500

    Deploy EC2 using Pulumi
    Add pair key

--- infrastructure/Program.cs
+++ infrastructure/Program.cs
@@ -1,26 +1,4 @@
-﻿using Pulumi;
-using Pulumi.Aws.Ec2;
-using Pulumi.Aws.Ec2.Inputs;
-using System.Collections.Generic;
+﻿using mundosE;
+using Pulumi;
 
-return await Deployment.RunAsync(() =>
-{
-   var ami = GetAmi.Invoke(new GetAmiInvokeArgs 
-   {
-       Owners = { },
-       MostRecent = true,
-       Filters = {
-           new GetAmiFilterArgs 
-           {
-               Name = "name",
-               Values = { "amzn-ami-hvm-*"}
-           }
-       }
-   });
-
-   // Export the name of the bucket
-   return new Dictionary<string, object?>
-   {
-      //["bucketName"] = bucket.Id
-   };
-});
+return await Deployment.RunAsync<ServerStack>();
---
+++ infrastructure/ServerStack.cs
@@ -0,0 +1,65 @@
+﻿using Pulumi;
+using Pulumi.Aws.Ec2;
+using Pulumi.Aws.Ec2.Inputs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mundosE
+{
+    public class ServerStack : Stack
+    {
+        private const string SIZE = "t2.micro";
+
+        [Output]
+        public Output<string> PublicIP { get; private set; }
+
+        [Output]
+        public Output<string> PublicDNS { get; private set; }
+
+        public ServerStack()
+        {
+            var ami = GetAmi.Invoke(new GetAmiInvokeArgs 
+            {
+                MostRecent = true,
+                Owners = { "137112412989" },
+                Filters = { new GetAmiFilterInputArgs { Name = "name", Values = "amzn-ami-hvm-*" } }
+            });
+
+            var group = new SecurityGroup("web-secgrp", new SecurityGroupArgs
+            {
+                Description = "Enable SSH",
+                Ingress = 
+                {
+                    new SecurityGroupIngressArgs
+                    {
+                        Protocol = "tcp",
+                        FromPort = 22,
+                        ToPort = 22,
+                        CidrBlocks = { "0.0.0.0/0" }
+                    }
+                }
+            });
+
+            var server = new Instance("deploy-server", new InstanceArgs 
+            {
+                InstanceType = SIZE,
+                VpcSecurityGroupIds = { group.Id},
+                UserData = "",
+                Ami = ami.Apply(a => a.Id)
+            });
+
+            var tag = new Tag("name", new() 
+            {
+                ResourceId = server.Id,
+                Key = "Name",
+                Value = "MundosE"
+            });
+
+            PublicIP = server.PublicIp;
+            PublicDNS = server.PublicDns;
+        }
+    }
+}
