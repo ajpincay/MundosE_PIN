33f0c67772660f44ee95432b8f3b7982edf76bb6
Angel Pincay <angel.pincay@intrado.com>
2022-08-28 19:30:40 -0500

    .

--- infrastructure/ServerStack.cs
+++ infrastructure/ServerStack.cs
@@ -24,8 +24,8 @@ namespace mundosE
             var ami = GetAmi.Invoke(new GetAmiInvokeArgs 
             {
                 MostRecent = true,
-                Owners = { "137112412989" },
-                Filters = { new GetAmiFilterInputArgs { Name = "name", Values = "amzn-ami-hvm-*" } }
+                Owners = { "099720109477" },
+                Filters = { new GetAmiFilterInputArgs { Name = "name", Values = "ubuntu/images/hvm-ssd/ubuntu-focal-20.04-amd64-server-20220610" } }
             });
 
             var group = new SecurityGroup("web-secgrp", new SecurityGroupArgs
@@ -39,6 +39,51 @@ namespace mundosE
                         FromPort = 22,
                         ToPort = 22,
                         CidrBlocks = { "0.0.0.0/0" }
+                    },
+                    new SecurityGroupIngressArgs
+                    {
+                        Protocol = "tcp",
+                        FromPort = 8000,
+                        ToPort = 8000,
+                        CidrBlocks = { "0.0.0.0/0" }
+                    },
+                    new SecurityGroupIngressArgs
+                    {
+                        Protocol = "tcp",
+                        FromPort = 9000,
+                        ToPort = 9000,
+                        CidrBlocks = { "0.0.0.0/0" }
+                    },
+                    new SecurityGroupIngressArgs
+                    {
+                        Protocol = "tcp",
+                        FromPort = 8080,
+                        ToPort = 8080,
+                        CidrBlocks = { "0.0.0.0/0" }
+                    },
+                    new SecurityGroupIngressArgs
+                    {
+                        Protocol = "tcp",
+                        FromPort = 8443,
+                        ToPort = 8443,
+                        CidrBlocks = { "0.0.0.0/0" }
+                    },
+                    new SecurityGroupIngressArgs
+                    {
+                        Protocol = "tcp",
+                        FromPort = 5432,
+                        ToPort = 5432,
+                        CidrBlocks = { "0.0.0.0/0" }
+                    }
+                },
+                Egress = 
+                {
+                    new SecurityGroupEgressArgs
+                    {
+                       Protocol = "-1",
+                       ToPort = 0,
+                       FromPort=0,
+                       CidrBlocks = { "0.0.0.0/0" }
                     }
                 }
             });
